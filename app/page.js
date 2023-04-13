5c91143d5dbca8bf2cdd559234b3213b202f298e
vidhya1706 <vidhya.b@myglamm.com>
2023-04-13 20:21:39 +0530

    removed static txt

--- app/page.js
+++ app/page.js
@@ -11,7 +11,6 @@ export default async function Home() {
 
   return (
     <main className="container mx-auto">
-      <h1 className="text-center text-4xl font-bold m-10">NextGram</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 auto-rows-max	 gap-6 m-10">
         {products.map((product) => (
           <Link key={product.id} href={`/product/${product.id}`}>
