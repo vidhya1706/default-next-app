bd2e1772e71be37745615d02ab7dbc1dac5bbc5b
vidhya1706 <vidhya.b@myglamm.com>
2023-04-13 19:45:32 +0530

    ui update

--- app/page.js
+++ app/page.js
@@ -12,16 +12,17 @@ export default async function Home() {
   return (
     <main className="container mx-auto">
       <h1 className="text-center text-4xl font-bold m-10">NextGram</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 auto-rows-max	 gap-6 m-10">
-        {products.map(({ id, image }) => (
-          <Link key={id} href={`/product/${id}`}>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 auto-rows-max	 gap-6 m-10">
+        {products.map((product) => (
+          <Link key={product.id} href={`/product/${product.id}`}>
             <Image
               alt=""
-              src={image}
+              src={product.image}
               height={500}
               width={500}
               className="w-full object-cover aspect-square"
             />
+            <h3 className="mt-2">{product.title}</h3>
           </Link>
         ))}
       </div>
